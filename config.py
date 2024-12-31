295f2ead37bafb6309d75d42b38d24ccb5c461d9
DarkBjs <jaychonkar617@gmail.com>
2024-12-31 12:05:40 +0530

    config.py

--- config.py
+++ config.py
@@ -26,8 +26,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6918300873"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://jaychonkar57:08r9haK7qmOm8Ykg@flexgpt.err87.mongodb.net/?retryWrites=true&w=majority&appName=Flexgpt")
-DB_NAME = os.environ.get("DATABASE_NAME", "Flexgpt")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://flexcoder:Guw67303@file.xpdqt.mongodb.net/?retryWrites=true&w=majority&appName=File")
+DB_NAME = os.environ.get("DATABASE_NAME", "file")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001909447082"))
